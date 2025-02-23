78ec5a06af6ba7a24dd0c02775b69b67cd825d99
Nicolas HOUDELOT <nicolas.houdelot@free.fr>
2025-02-23 15:47:29 +0100

    added : ozymandias-by-fcccf

---
+++ linux/demos/o/ozymandias-by-fcccf/debian/ozymandias-by-fcccf.6.md
@@ -0,0 +1,26 @@
+% ozymandias-by-fcccf(6) Ozymandias User Manuals
+% Nicolas HOUDELOT (nicolas@demosdebs.org),FCCCF
+% 2025-02-22
+
+# NAME
+ozymandias-by-fcccf - the command to run Ozymandias.
+
+# SYNOPSIS
+ozymandias-by-fcccf [*options*]
+
+# DESCRIPTION
+Ozymandias is a demo released by FCCCF in august 2019.
+11th pc demo at	Assembly 2019
+
+muffintrap [Code, graphics, design]
+basscadet [Music]
+
+# OPTIONS
+\--help
+:   Display help for the command
+
+# INFOS
+https://www.pouet.net/prod.php?which=82453
+
+# BUGS
+No known bugs.
---
+++ linux/demos/o/ozymandias-by-fcccf/debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/ozymandias-by-fcccf/issues
+# Bug-Submit: https://github.com/<user>/ozymandias-by-fcccf/issues/new
+# Changelog: https://github.com/<user>/ozymandias-by-fcccf/blob/master/CHANGES
+# Documentation: https://github.com/<user>/ozymandias-by-fcccf/wiki
+# Repository-Browse: https://github.com/<user>/ozymandias-by-fcccf
+# Repository: https://github.com/<user>/ozymandias-by-fcccf.git
