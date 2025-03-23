00ef428c76e84ff8d4b70f70c29a0bb837a49b18
Nicolas HOUDELOT <nicolas.houdelot@free.fr>
2025-03-23 01:56:37 +0100

    added : nextlevel-by-pixdigit

---
+++ linux/demos/n/nextlevel-by-pixdigit/debian/nextlevel-by-pixdigit.6.md
@@ -0,0 +1,25 @@
+% nextlevel-by-pixdigit(6) Next Level User Manuals
+% Nicolas HOUDELOT (nicolas@demosdebs.org),Pixdigit
+% 2025-03-22
+
+# NAME
+nextlevel-by-pixdigit - the command to run Next Level.
+
+# SYNOPSIS
+nextlevel-by-pixdigit [*options*]
+
+# DESCRIPTION
+Next Level is a demo released by Pixdigit in august 2019.
+Ranked 7th pc demo at Evoke 2019
+
+Pixdigit [Code, Graphics]
+
+# OPTIONS
+\--help
+:   Display help for the command
+
+# INFOS
+https://www.pouet.net/prod.php?which=82674
+
+# BUGS
+No known bugs.
---
+++ linux/demos/n/nextlevel-by-pixdigit/debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/nextlevel-by-pixdigit/issues
+# Bug-Submit: https://github.com/<user>/nextlevel-by-pixdigit/issues/new
+# Changelog: https://github.com/<user>/nextlevel-by-pixdigit/blob/master/CHANGES
+# Documentation: https://github.com/<user>/nextlevel-by-pixdigit/wiki
+# Repository-Browse: https://github.com/<user>/nextlevel-by-pixdigit
+# Repository: https://github.com/<user>/nextlevel-by-pixdigit.git
