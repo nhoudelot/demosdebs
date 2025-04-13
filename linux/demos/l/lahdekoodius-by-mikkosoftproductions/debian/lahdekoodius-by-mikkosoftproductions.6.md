4d15b320e8bff0d33c1477151d00453f637950e1
Nicolas HOUDELOT <nicolas.houdelot@free.fr>
2025-04-13 15:44:17 +0200

    added : lahdekoodius-by-mikkosoftproductions

---
+++ linux/demos/l/lahdekoodius-by-mikkosoftproductions/debian/lahdekoodius-by-mikkosoftproductions.6.md
@@ -0,0 +1,26 @@
+% lahdekoodius-by-mikkosoftproductions(6) Lahdekoodius User Manuals
+% Nicolas HOUDELOT (nicolas@demosdebs.org),Mikkosoft Productions
+% 2025-04-13
+
+# NAME
+lahdekoodius-by-mikkosoftproductions - the command to run Lahdekoodius.
+
+# SYNOPSIS
+lahdekoodius-by-mikkosoftproductions [*options*]
+
+# DESCRIPTION
+Lahdekoodius is a demo released by Mikkosoft Productions in september 2019.
+Ranked 2nd combined demo/intro at Skrolli Party 2019
+
+tdb [Code, graphics]
+Thihi [Music]
+
+# OPTIONS
+\--help
+:   Display help for the command
+
+# INFOS
+https://www.pouet.net/prod.php?which=83105
+
+# BUGS
+No known bugs.
---
+++ linux/demos/l/lahdekoodius-by-mikkosoftproductions/debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/lahdekoodius-by-mikkosoftproductions/issues
+# Bug-Submit: https://github.com/<user>/lahdekoodius-by-mikkosoftproductions/issues/new
+# Changelog: https://github.com/<user>/lahdekoodius-by-mikkosoftproductions/blob/master/CHANGES
+# Documentation: https://github.com/<user>/lahdekoodius-by-mikkosoftproductions/wiki
+# Repository-Browse: https://github.com/<user>/lahdekoodius-by-mikkosoftproductions
+# Repository: https://github.com/<user>/lahdekoodius-by-mikkosoftproductions.git
